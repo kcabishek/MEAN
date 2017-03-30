fb182c7edecd853bc70e6ba2fe255bd71e3f86cc
Abishek KC <kcabishek@gmail.com>
2017-03-30 02:44:00 -0400

    Add service class

---
+++ src/messages/message.service.ts
@@ -0,0 +1,18 @@
+import {Message} from "./message.model";
+
+
+export class MessageService{
+    messages: Message[] = [];
+
+    addMessage(message: Message){
+      this.messages.push(message);
+    }
+
+    getMessage(){
+      return this.messages;
+    }
+
+    deleteMessage(message: Message){
+      this.messages.splice(this.messages.indexOf(message), 1);
+    }
+}
